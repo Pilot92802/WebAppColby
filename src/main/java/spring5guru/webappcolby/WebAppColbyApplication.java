98331504c2da50e1be4531f57057a071ff54d577
Pilot92802 <colby.downsbb@yahoo.com>
2021-10-03 17:07:37 -0500

    Initial commit

---
+++ src/main/java/spring5guru/webappcolby/WebAppColbyApplication.java
@@ -0,0 +1,13 @@
+package spring5guru.webappcolby;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebAppColbyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebAppColbyApplication.class, args);
+    }
+
+}
