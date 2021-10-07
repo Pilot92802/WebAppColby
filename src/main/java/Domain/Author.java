87826e9280b818519a6d46fc89c5608ca31428e1
Pilot92802 <colby.downsbb@yahoo.com>
2021-10-06 23:51:15 -0500

    updated book and author with JPA annotations and associations

--- src/main/java/Domain/Author.java
+++ src/main/java/Domain/Author.java
@@ -1,17 +1,18 @@
 package Domain;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.util.Set;
 
 @Entity
 public class Author {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
     private String firstName;
     private String lastName;
+    @ManyToMany(mappedBy = "authors")
     private Set<Book> books;
 
     public Author() {
@@ -21,6 +22,14 @@ public class Author {
         this.firstName = firstName;
         this.lastName = lastName;
         this.books = books;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public String getFirstName() {
