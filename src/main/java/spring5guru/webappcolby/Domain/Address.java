24ec2829ec1323fd01c0cef8066256018fc8d1ce
Pilot92802 <colby.downsbb@yahoo.com>
2021-10-08 19:26:53 -0500

    assignment 2 add publisher

---
+++ src/main/java/spring5guru/webappcolby/Domain/Address.java
@@ -0,0 +1,59 @@
+package spring5guru.webappcolby.Domain;
+
+public class Address {
+
+    private String addressLine1;
+    private String city;
+    private String state;
+    private String zip;
+
+    public Address() {
+    }
+
+    public Address(String addressLine1, String city, String state, String zip) {
+        this.addressLine1 = addressLine1;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+    }
+
+    public String getAddressLine1() {
+        return addressLine1;
+    }
+
+    public void setAddressLine1(String addressLine1) {
+        this.addressLine1 = addressLine1;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getZip() {
+        return zip;
+    }
+
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
+
+    @Override
+    public String toString() {
+        return "Address Line 1: " + addressLine1 +
+                " City: " + city +
+                " State: " + state +
+                " Zip: " + zip + '\'';
+    }
+}
